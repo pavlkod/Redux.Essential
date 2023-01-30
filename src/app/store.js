b9cd9969b3e0e379febed1f812086077da041478
Pavel.K <pavelweb74@gmail.com>
2023-01-30 23:11:06 +0500

    Add apiSlice in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 
-import postReducer from "../components/Posts/slicePosts";
-import userReducer from "../components/User/sliceUser";
-import notificationReducer from "../components/Notifications/sliceNotification";
+import postReducer from '../components/Posts/slicePosts'
+import userReducer from '../components/User/sliceUser'
+import notificationReducer from '../components/Notifications/sliceNotification'
+
+import { apiSlice } from '../api/apiSlice'
 
 export default configureStore({
   reducer: {
     posts: postReducer,
     users: userReducer,
-    notifications: notificationReducer
+    notifications: notificationReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  devTools: { trace: true, traceLimit: 25 }
-});
+  devTools: { trace: true, traceLimit: 25 },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+})
