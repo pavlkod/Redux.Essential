7aa578a60308ec3c9c6104beb68e639250653f2a
Pavel.K <pavelweb74@gmail.com>
2023-02-02 20:57:21 +0500

    add useGetPostQuery and useUpdatePostMutation in EditPost

--- src/components/EditPost/index.js
+++ src/components/EditPost/index.js
@@ -1,34 +1,39 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { useGetPostQuery, useUpdatePostMutation } from '../../api/apiSlice'
 
-import { updatePost } from "../Posts/slicePosts";
+// import { updatePost } from '../Posts/slicePosts'
+import { selectAllUsers } from '../User/sliceUser'
 
 export const EditPostForm = ({ history, match }) => {
-  const { postId } = match.params;
+  const { postId } = match.params
 
-  const post = useSelector((state) =>
-    state.posts.find((post) => post.id === postId)
-  );
-  const users = useSelector((state) => state.users);
+  const { data: post } = useGetPostQuery(postId)
+  const [updatePost, { isLoading }] = useUpdatePostMutation()
 
-  const [title, setTitle] = useState(post.title);
-  const [content, setContent] = useState(post.content);
-  const [userId, setUserId] = useState(post.user || "0");
+  // const post = useSelector((state) =>
+  //   state.posts.find((post) => post.id === postId)
+  // );
+  const users = useSelector(selectAllUsers)
 
-  const dispatch = useDispatch();
+  const [title, setTitle] = useState(post.title)
+  const [content, setContent] = useState(post.content)
+  const [userId, setUserId] = useState(post.user || '0')
+
+  const dispatch = useDispatch()
   // const history = useHistory();
 
-  const onTitleChanged = (e) => setTitle(e.target.value);
-  const onContentChanged = (e) => setContent(e.target.value);
-  const onAuthorChanged = (e) => setUserId(e.target.value);
+  const onTitleChanged = (e) => setTitle(e.target.value)
+  const onContentChanged = (e) => setContent(e.target.value)
+  const onAuthorChanged = (e) => setUserId(e.target.value)
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(updatePost({ id: postId, title, content, userId }));
-      history.push(`/posts/${postId}`);
+      updatePost({ id: postId, title, content, user: users[userId] })
+      history.push(`/posts/${postId}`)
     }
-  };
+  }
 
   return (
     <section>
@@ -52,7 +57,7 @@ export const EditPostForm = ({ history, match }) => {
         />
         <label htmlFor="user">User</label>
         <select id="user" value={userId} onChange={onAuthorChanged}>
-          {users.map((user) => (
+          {Object.values(users).map((user) => (
             <option key={user.id} value={user.id}>
               {user.name}
             </option>
@@ -63,5 +68,5 @@ export const EditPostForm = ({ history, match }) => {
         Save Post
       </button>
     </section>
-  );
-};
+  )
+}
