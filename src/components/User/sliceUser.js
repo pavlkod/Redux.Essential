70ae15a61580b57665d7d2c3ce0760fbe5ed67c4
Pavel.K <pavelweb74@gmail.com>
2023-02-02 20:58:59 +0500

    add apiSlice.endpoints.getUsers in slideUser

--- src/components/User/sliceUser.js
+++ src/components/User/sliceUser.js
@@ -1,35 +1,50 @@
 import {
   createSlice,
   createAsyncThunk,
-  createEntityAdapter
-} from "@reduxjs/toolkit";
-import { client } from "../../api/client";
+  createEntityAdapter,
+  createSelector,
+} from '@reduxjs/toolkit'
+import { apiSlice } from '../../api/apiSlice'
+import { client } from '../../api/client'
 
-const usersAdapter = createEntityAdapter();
-const initialState = usersAdapter.getInitialState();
+const usersAdapter = createEntityAdapter()
+const initialState = usersAdapter.getInitialState()
 
-export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
-  const response = await client.get("/fakeApi/users");
-  return response.data;
-});
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('/fakeApi/users')
+  return response.data
+})
 
 const usersSlice = createSlice({
-  name: "users",
+  name: 'users',
   initialState,
   reducers: {},
   extraReducers(build) {
-    build.addCase(fetchUsers.fulfilled, usersAdapter.setAll);
-  }
-});
+    build.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
+  },
+})
 
-export const selectAllUsers2 = (state) => state.users;
+export const selectAllUsers2 = (state) => state.users
 export const selectUserById2 = (state, id) =>
-  state.users.find((user) => user.id === id);
+  state.users.find((user) => user.id === id)
 
-export default usersSlice.reducer;
+export default usersSlice.reducer
 
 export const {
-  selectAll: selectAllUsers,
-  selectById: selectUserById,
-  selectIds: selectUserIds
-} = usersAdapter.getSelectors((state) => state.users);
+  selectAll: selectAllUsers3,
+  selectById: selectUserById3,
+  selectIds: selectUserIds,
+} = usersAdapter.getSelectors((state) => state.users)
+
+const emptyUsers = []
+const selectUsersResult = apiSlice.endpoints.getUsers.select()
+
+export const selectAllUsers = createSelector(
+  selectUsersResult,
+  (users) => users?.data ?? emptyUsers
+)
+export const selectUserById = createSelector(
+  selectAllUsers,
+  (state, userId) => userId,
+  (users, id) => users.find((user) => user.id === id)
+)
