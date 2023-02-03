1dfe1629a7ebea9dd79c145df4e9d39a1390df44
Pavel.K <pavelweb74@gmail.com>
2023-02-03 22:22:23 +0500

    add getNotifications in notificationSlice

--- src/components/Notifications/sliceNotification.js
+++ src/components/Notifications/sliceNotification.js
@@ -1,59 +1,129 @@
 import {
   createSlice,
   createAsyncThunk,
-  createEntityAdapter
-} from "@reduxjs/toolkit";
+  createEntityAdapter,
+  createSelector,
+} from '@reduxjs/toolkit'
+import { apiSlice } from '../../api/apiSlice'
 
-import { client } from "../../api/client";
+import { client } from '../../api/client'
+import { forceGenerateNotifications } from '../../api/server'
 
 export const fetchNotifications = createAsyncThunk(
-  "notifications/fetchNotifications",
+  'notifications/fetchNotifications',
   async (_, { getState }) => {
-    const allNotifications = selectAllNotifications(getState());
-    const [latestNotification] = allNotifications;
-    const latestTimestamp = latestNotification ? latestNotification.date : "";
+    const allNotifications = selectAllNotifications(getState())
+    const [latestNotification] = allNotifications
+    const latestTimestamp = latestNotification ? latestNotification.date : ''
     const response = await client.get(
       `/fakeApi/notifications?since=${latestTimestamp}`
-    );
-    return response.data;
+    )
+    return response.data
   }
-);
+)
 
 const notificationsAdapter = createEntityAdapter({
-  sortComparer: (a, b) => b.date.localeCompare(a.date)
-});
+  sortComparer: (a, b) => b.date.localeCompare(a.date),
+})
 
 const notificationsSlice = createSlice({
-  name: "notifications",
+  name: 'notifications',
   initialState: notificationsAdapter.getInitialState(),
   reducers: {
     allNotificationsRead(state, action) {
       Object.values(state.entities).forEach((notification) => {
-        notification.read = true;
-      });
-    }
+        notification.read = true
+      })
+    },
   },
   extraReducers(builder) {
     builder.addCase(fetchNotifications.fulfilled, (state, action) => {
-      notificationsAdapter.addOne(state, action.payload);
+      notificationsAdapter.addOne(state, action.payload)
       Object.values(state.entities).forEach((notification) => {
         // Any notifications we've read are no longer new
-        notification.isNew = !notification.read;
-      });
-    });
-  }
-});
+        notification.isNew = !notification.read
+      })
+    })
+  },
+})
 
-export const { allNotificationsRead } = notificationsSlice.actions;
+export const { allNotificationsRead } = notificationsSlice.actions
 
-export default notificationsSlice.reducer;
+export default notificationsSlice.reducer
 
-export const selectAllNotifications2 = (state) => state.notifications;
+export const selectAllNotifications2 = (state) => state.notifications
 
 // Export the customized selectors for this adapter using `getSelectors`
 export const {
-  selectAll: selectAllNotifications
+  selectAll: selectAllNotifications,
   // selectById: selectPostById,
   // selectIds: selectPostIds
   // Pass in a selector that returns the posts slice of state
-} = notificationsAdapter.getSelectors((state) => state.notifications);
+} = notificationsAdapter.getSelectors((state) => state.notifications)
+
+export const extendedApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getNotifications: builder.query({
+      query: () => '/notifications',
+      async onCacheEntryAdded(
+        arg,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) {
+        // create a websocket connection when the cache subscription starts
+        const ws = new WebSocket('ws://localhost')
+        try {
+          // wait for the initial query to resolve before proceeding
+          await cacheDataLoaded
+
+          // when data is received from the socket connection to the server,
+          // update our query result with the received message
+          const listener = (event) => {
+            const message = JSON.parse(event.data)
+            switch (message.type) {
+              case 'notifications': {
+                updateCachedData((draft) => {
+                  // Insert all received notifications from the websocket
+                  // into the existing RTKQ cache array
+                  draft.push(...message.payload)
+                  draft.sort((a, b) => b.date.localeCompare(a.date))
+                })
+                break
+              }
+              default:
+                break
+            }
+          }
+
+          ws.addEventListener('message', listener)
+        } catch {
+          // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`,
+          // in which case `cacheDataLoaded` will throw
+        }
+        // cacheEntryRemoved will resolve when the cache subscription is no longer active
+        await cacheEntryRemoved
+        // perform cleanup steps once the `cacheEntryRemoved` promise resolves
+        ws.close()
+      },
+    }),
+  }),
+})
+
+export const { useGetNotificationsQuery } = extendedApi
+
+const emptyNotifications = []
+
+export const selectNotificationsResult =
+  extendedApi.endpoints.getNotifications.select()
+
+const selectNotificationsData = createSelector(
+  selectNotificationsResult,
+  (notificationsResult) => notificationsResult.data ?? emptyNotifications
+)
+
+export const fetchNotificationsWebsocket = () => (dispatch, getState) => {
+  const allNotifications = selectNotificationsData(getState())
+  const [latestNotification] = allNotifications
+  const latestTimestamp = latestNotification?.date ?? ''
+  // Hardcode a call to the mock server to simulate a server push scenario over websockets
+  forceGenerateNotifications(latestTimestamp)
+}
